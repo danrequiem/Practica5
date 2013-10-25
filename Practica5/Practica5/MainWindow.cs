b8c2dc473482a94d72f474cf028a1b062b536b96
danrequiem <DAN@iMac-de-Daniel.local>
2013-10-24 22:00:33 -0500

    Se agrego la funcion del boton convertir y la funcion limpar.

---
+++ Practica5/Practica5/MainWindow.cs
@@ -0,0 +1,52 @@
+using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnConvertirClicked (object sender, EventArgs e)
+	{
+
+		double pesos,cambio;
+
+		bool conversionPesos =Double.TryParse(PesosText.Text, out pesos);
+		bool conversionCambio =Double.TryParse(CambioText.Text, out cambio);
+
+		if( !conversionPesos || !conversionCambio || pesos == 0 || cambio == 0){
+
+			MessageDialog Mensaje = new MessageDialog(
+										
+									null,
+									DialogFlags.Modal,
+									MessageType.Error,
+									ButtonsType.None,
+									"Error"
+				);
+			Mensaje.Show();
+
+		}
+		else{
+
+			double cambiado = pesos / cambio;
+			DolatesText.Text = cambiado.ToString();
+
+		} 
+
+	}
+
+	protected void OnLimpiarClicked (object sender, EventArgs e)
+	{
+		PesosText.Text = "";
+		CambioText.Text = "";
+	}
+}
